dba17af87565f8baf1d2db191554cd2bd18880a7
吉原　史朗 <>
2018-10-17 16:45:13 +0900

    ページ遷移の確認

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -17,3 +17,8 @@ class QuotesSpider(scrapy.Spider):
             item['date']   = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
 
         yield item
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback = self.parse)
